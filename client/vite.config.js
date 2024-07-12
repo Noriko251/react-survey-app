523f56571fc599433cc6cc16e1658bb9847099b1
Nonko Taniguchi <studio-nonko@hotmail.co.jp>
2024-07-12 15:43:57 -0400

    Initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  base: "/react-survey-app/"
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
       'process.env.SIGNIN_USERNAME': JSON.stringify(env.SIGNIN_USERNAME),
       'process.env.SIGNIN_PASSWORD': JSON.stringify(env.SIGNIN_PASSWORD)
     },
+    base: "/react-survey-app/",
     plugins: [react()],
   }
 })
